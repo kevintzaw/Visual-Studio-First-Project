6d38094f39de30fd3062d5fa41847ef978b31d83
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-07 19:04:20 -0800

    Add project files.

---
+++ ContosoCrafts Website/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var up = document.getElementById('GFG_UP');
+var down = document.getElementById('GFG_DOWN');
+var div = document.getElementById('GFG_DIV');
+up.innerHTML = "Click on button to remove the element.";
+
+function GFG_Fun() {
+    div.parentNode.removeChild(div);
+    down.innerHTML = "Element is removed.";
+}
